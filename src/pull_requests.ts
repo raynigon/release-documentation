1465f7b632d9a8c102ce1c5a624bc8a8cf1078e4
Simon Schneider <70256727+simon-schneider-gcx@users.noreply.github.com>
2021-05-19 13:52:25 +0200

    Add Logging

--- src/pull_requests.ts
+++ src/pull_requests.ts
@@ -2,6 +2,7 @@ import * as github from '@actions/github';
 import * as core from '@actions/core';
 
 async function fetchPullRequest(token: string, pr: string): Promise<any> {
+    core.info(`fetch ${pr}`);
     const oktokit = github.getOctokit(token);
     const response: any = await oktokit.graphql(`query {
         repository(owner: "${github.context.repo.owner}", name: "${github.context.repo.repo}") {
@@ -19,6 +20,7 @@ async function fetchPullRequest(token: string, pr: string): Promise<any> {
             }
         }
       }`)
+    core.info(`received ${pr}: ${response}`);
     return {
         "id": response["data"]["repository"]["id"],
         "title": response["data"]["repository"]["title"],
@@ -27,6 +29,7 @@ async function fetchPullRequest(token: string, pr: string): Promise<any> {
 }
 
 export async function createTemplateContext(token: string, prs: Array<string>): Promise<any> {
+    core.info("start fetch");
     const pullRequest = await Promise.all(prs.map(it=>fetchPullRequest(token, it)));
     core.info(JSON.stringify(pullRequest));
     return {};
