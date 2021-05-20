68d7e1a8c585e08679c86db9ae5a2c29f00d79df
Simon Schneider <70256727+simon-schneider-gcx@users.noreply.github.com>
2021-05-20 17:57:16 +0200

    Improve Template Context

--- src/pull_requests.ts
+++ src/pull_requests.ts
@@ -2,7 +2,7 @@ import * as github from '@actions/github';
 import * as core from '@actions/core';
 
 async function fetchPullRequest(token: string, pr: string): Promise<any> {
-  core.info(`fetch ${pr}`);
+  core.debug(`Fetch Pull Request ${pr}`);
   const oktokit = github.getOctokit(token);
   const response: any = await oktokit.graphql(`query {
         repository(owner: "${github.context.repo.owner}", name: "${github.context.repo.repo}") {
@@ -20,7 +20,7 @@ async function fetchPullRequest(token: string, pr: string): Promise<any> {
             }
         }
       }`)
-  core.info(`received ${pr}: ${JSON.stringify(response)}`);
+  core.debug(`Received Pull Request ${pr}: ${JSON.stringify(response)}`);
   const pullRequest = response["repository"]["pullRequest"]
   return {
     "id": pullRequest["id"],
@@ -30,8 +30,20 @@ async function fetchPullRequest(token: string, pr: string): Promise<any> {
 }
 
 export async function createTemplateContext(token: string, prs: Array<string>): Promise<any> {
-  core.info("start fetch");
-  const pullRequest = await Promise.all(prs.map(it => fetchPullRequest(token, it)));
-  core.info(JSON.stringify(pullRequest));
-  return {};
+  core.debug("Creating Template Context");
+  const pullRequests = await Promise.all(prs.map(it => fetchPullRequest(token, it)));
+  const labels = [...new Set(pullRequests.flatMap(it => it.labels))]
+  const context: any = {
+    "pull_requests": {
+      "_all": pullRequests
+    }
+  };
+  labels.forEach(it => context["pull_requests"][it] = [])
+  pullRequests.forEach(it => {
+    for (const label of it.labels){
+      context["pull_requests"][label].push(it)
+    }
+  })
+  core.debug(`Template Context: ${JSON.stringify(context)}`);
+  return context
 }
