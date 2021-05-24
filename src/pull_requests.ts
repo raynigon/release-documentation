a0e0b446632b242b174455c2fc5cc2913e1ae90e
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2021-05-24 21:24:12 +0200

    Update Pull Request Sorting

--- src/pull_requests.ts
+++ src/pull_requests.ts
@@ -1,7 +1,19 @@
 import * as github from '@actions/github';
 import * as core from '@actions/core';
 
-async function fetchPullRequest(token: string, pr: string): Promise<any> {
+interface PullRequest {
+  id: string
+  number: number
+  title: string,
+  state: string,
+  labels: Array<string>
+}
+
+interface TemplateContext {
+  pull_requests: { [key: string]: Array<PullRequest> },
+}
+
+async function fetchPullRequest(token: string, pr: string): Promise<PullRequest> {
   core.debug(`Fetch Pull Request ${pr}`);
   const oktokit = github.getOctokit(token);
   const response: any = await oktokit.graphql(`query {
@@ -27,25 +39,27 @@ async function fetchPullRequest(token: string, pr: string): Promise<any> {
     "id": pullRequest["id"],
     "number": pullRequest["number"],
     "title": pullRequest["title"],
+    "state": pullRequest["state"], 
     "labels": (pullRequest["labels"]["edges"] as Array<any>).map(it => it["node"]["name"])
   }
 }
 
 export async function createTemplateContext(token: string, prs: Array<string>): Promise<any> {
   core.debug("Creating Template Context");
-  const pullRequests = await Promise.all(prs.map(it => fetchPullRequest(token, it)));
+  const pullRequests = (await Promise.all(prs.map(it => fetchPullRequest(token, it)))).filter(pr=>pr.state != "MERGED");
   const labels = [...new Set(pullRequests.flatMap(it => it.labels))]
-  const context: any = {
+  const context: TemplateContext = {
     "pull_requests": {
       "_all": pullRequests
     }
   };
-  labels.forEach(it => context["pull_requests"][it] = [])
+  labels.forEach(it => context.pull_requests[it] = [])
   pullRequests.forEach(it => {
-    for (const label of it.labels){
-      context["pull_requests"][label].push(it)
+    for (const label of it.labels) {
+      context.pull_requests[label].push(it)
     }
-  })
+  });
+  labels.forEach(it => context.pull_requests[it].sort((a, b) => a.number - b.number))
   core.debug(`Template Context: ${JSON.stringify(context)}`);
   return context
 }
